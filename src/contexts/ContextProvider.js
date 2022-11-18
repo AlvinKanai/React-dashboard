791e674d1874618601afadc5cc8f7b10639777c4
Alvin Kanai <alvinkanai@gmail.com>
2022-11-19 00:28:25 +0300

    More hooks

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState } from "react";
 const StateContext = createContext()
 
-const InitialState = {
+const initialState = {
     chat: false,
     cart: false,
     userProfile: false,
@@ -10,9 +10,14 @@ const InitialState = {
 
 export const ContextProvider = ({children}) => {
     const [activeMenu, setActiveMenu] = useState (true)
+    const [isClicked, setIsClicked] = useState(initialState)
+    const handleClick = (clicked) => {
+        setIsClicked({...initialState, [clicked]: true})
+    }
+    const [screenSize, setScreenSize] = useState(undefined) 
 
     return ( 
-        <StateContext.Provider value={{activeMenu, }}>
+        <StateContext.Provider value={{activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize}}>
             {children}
         </StateContext.Provider>
     )
